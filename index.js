8a4aac94226351ce40d9193fe5ce827efd00f328
Farida <faridah.absi@gmail.com>
2018-09-06 18:39:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file
+function findMatching(array,arg){
+  let matching = [];
+  
+  for (const name of array){
+    if (name.toUpperCase() === arg.toUpperCase()){
+      
+      matching.push(name);
+    }
+    
+  }
+  return matching;
+  
+}
+ function fuzzyMatch(array, arg){
+  let matching = [];
+  let num = arg.length;
+  
+  for (const name of array){
+    
+    if (name.slice(0,num) === arg){
+      matching.push(name);
+    }
+  }
+  return matching;
+}
+ function matchName(array, arg){
+  let matching = [];
+  
+    for (const key of array){
+      
+      if (key["name"] === arg){
+        matching.push(key);
+      }
+      
+      
+    }
+  return matching;
+}
