8f77985e6713c61dcb29bf72c5de114bcfe773d5
Farida <faridah.absi@gmail.com>
2018-09-06 18:37:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file
+function findMatching(array,arg){
+  let matching = [];
+  
+  for (const name of array){
+    if (name.toUpperCase() === arg.toUpperCase()){
+      
+      matching.push(name);
+    }
+    
+  }
+  return matching;
+  
+}
+ function fuzzyMatch(array, arg){
+  let matching = [];
+  let num = arg.length;
+  
+  for (const name of array){
+    
+    if (name.slice(0,num) === arg){
+      matching.push(name);
+    }
+  }
+  return matching;
+}
+ function matchName(array, arg){
+  let matching = [];
+  
+    for (const user of array){
+      
+      if (user["name"] === arg){
+        matching.push(user);
+      }
+      
+      
+    }
+  return matching;
+}
